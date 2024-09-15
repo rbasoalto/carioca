d3b2b8e8c450c925bfee4df039531806b4aeab28
Rodrigo Basoalto <rodrigo@basoalto.cl>
2024-09-15 20:20:38 -0300

    5th iteration
    progressive web app

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 Made 100% by ChatGPT, using o1-preview. Commits are iterations in the chat.
 
-[Link to ChatGPT chat](https://chatgpt.com/share/66e49bb0-537c-800d-a8cd-049802a93848)
+[Link to ChatGPT chat](https://chatgpt.com/share/66e76ba3-cee8-800d-94eb-59b60ab91075)
---
+++ sw.js
@@ -0,0 +1,50 @@
+const CACHE_NAME = 'carioca-score-cache-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './app.js',
+  './manifest.json',
+  './icons/icon-192.png',
+  './icons/icon-512.png',
+  // Add any other assets you want to cache
+];
+
+self.addEventListener('install', function(event) {
+  // Perform install steps
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        // Cache hit - return the response
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
+});
+
+self.addEventListener('activate', function(event) {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.map(function(cacheName) {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
